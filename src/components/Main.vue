3e63868a9a3aed03b900ed7b850af1e70950cdd7
Iván Juárez Núñez <ivan@blindgazer.com>
2018-01-28 20:07:25 -0600

    Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>CodeSandbox VApp</h1>
+</template>
+
+<script>
+export default {
+name: 'main',
+  data () {
+    return {
+      msg: 'CodeSandbox'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
